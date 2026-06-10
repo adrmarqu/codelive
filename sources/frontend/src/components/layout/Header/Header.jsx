8d45c628e29347e44b829e6d671529a0a371068e
adrmarqu <adria.marquez.3b@gmail.com>
2026-06-10 18:19:54 +0200

    page height

--- sources/frontend/src/components/layout/Header/Header.jsx
+++ sources/frontend/src/components/layout/Header/Header.jsx
@@ -69,6 +69,7 @@ function Header({role, setRole, setIsLogged})
     const userLinks = (
         <>
             <Link to={PATHS.USER.PROFILE}>{t('header.profile')}</Link>
+            <Link to={PATHS.SEE}>{t('header.courses')}</Link>
             <Link to={PATHS.PROGRESS}>{t('header.progress')}</Link>
             <Link to={PATHS.RANKING}>{t('header.ranking')}</Link>
         </>
