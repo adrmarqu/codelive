57aacacb770c3da0aedc70d0b640062a8f30aac5
Adrià Márquez Nebot <adria@Adriamn.local>
2026-06-12 12:41:53 +0200

    4

--- sources/backend/src/config/mailer.js
+++ sources/backend/src/config/mailer.js
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
     host:   process.env.SMTP_HOST || 'smtp.gmail.com',
     port:   parseInt(process.env.SMTP_PORT || '587'),
     secure: process.env.SMTP_SECURE !== "false",
-    /* family: 4, */
+    family: 4,
     auth:
     {
         user: process.env.SMTP_USER,
