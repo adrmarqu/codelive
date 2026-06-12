1afd7739e4a90b8850600ccc048eb71bd9695a0e
Adrià Márquez Nebot <adria@Adriamn.local>
2026-06-12 12:58:09 +0200

    ipv4 retry

--- sources/backend/src/config/mailer.js
+++ sources/backend/src/config/mailer.js
@@ -6,7 +6,9 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     host:   process.env.SMTP_HOST || 'smtp.gmail.com',
     port:   parseInt(process.env.SMTP_PORT || '587'),
-    secure: process.env.SMTP_SECURE !== "false" || false,
+    secure: process.env.SMTP_SECURE === "true" || process.env.SMTP_SECURE === true || (!process.env.SMTP_PORT),
+    
+    family: 4,
     auth:
     {
         user: process.env.SMTP_USER,
